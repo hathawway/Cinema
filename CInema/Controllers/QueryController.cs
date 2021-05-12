b96a0d475bda7e4865df45f440ba67da5a320091
Olya <hathawway@gmail.com>
2021-05-12 19:58:19 +0300

    формирование запросов

--- CInema/Controllers/QueryController.cs
+++ CInema/Controllers/QueryController.cs
@@ -1,8 +1,12 @@
 ﻿using Cinema.Domain.Db;
 using Cinema.Models;
+using Cinema.Models.Query;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,9 +15,11 @@ namespace Cinema.Controllers
     public class QueryController : Controller
     {
         private readonly CinemaDbContext _context;
-        public QueryController(CinemaDbContext context)
+        private readonly string ConnectionString;
+        public QueryController(CinemaDbContext context, IConfiguration Configuration)
         {
             _context = context;
+            ConnectionString = Configuration.GetConnectionString("OracleDBConnection");
         }
 
         [HttpGet]
@@ -35,7 +41,7 @@ namespace Cinema.Controllers
         public IActionResult ListFilms()
         {
             var russiaCode = _context.Countries.First(x => x.Name == "Россия").Kod;
-            var films = _context.Films.Where(x => x.StartDate.Year > 2021 && x.CountryKod == russiaCode).Select(x => new FilmViewModel
+            var films = _context.Films.Where(x => x.StartDate.Year == 2021 && x.CountryKod == russiaCode).Select(x => new FilmViewModel
             {
                 Id = x.Kod,
                 Country = _context.Countries.First(c => c.Kod == x.CountryKod).Name,
@@ -55,12 +61,45 @@ namespace Cinema.Controllers
         [HttpGet]
         public IActionResult ListRole()
         {
-            //TODO call procedure
+            OracleConnection con = new();
+            con.ConnectionString = ConnectionString;
+            con.Open();
+
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select kino.PACKAGE__P.spisokemp from dual";
+            List<List<string>> response = new();
+            var lenth = 1;
+            if (lenth > 0)
+            {
+                OracleDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    response.Add(new List<string>());
+                    for (int i = 0; i < lenth; i++)
+                    {
+                        response[^1].Add(reader.GetString(i));
+                    }
+                }
+            }
+            else
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            con.Close();
+
+            var result = response[0][0].Split("haha").ToArray();
+            result = result.Take(result.Length - 1).ToArray();
+            ViewData["TableData"] = result;
+            ViewData["Headers"] = new string[] { "Фильмы, в которых режиссер является одновременно " +
+                "исполнителем главной роли"};
             return View();
         }
 
         [HttpGet]
-        public IActionResult StatisticGenre()
+        public IActionResult StatisticsGenre()
         {
             var russiaCode = _context.Countries.First(x => x.Name == "Россия").Kod;
             var films = _context.Films.Select(x => x).ToArray();
@@ -70,11 +109,11 @@ namespace Cinema.Controllers
                 Genre = x.Key,
                 RuFilm = films.Where(f => f.GenreKod == x.Key && f.CountryKod == russiaCode).Count(),
                 Count = x.Count()
-            }).Select(x => new 
+            }).Select(x => new StatOnGenre
             {
                 Genre = _context.Genres.First(g => g.Kod == x.Genre).Name,
-                RuFilm = x.RuFilm,
-                Count = x.Count - x.RuFilm
+                RusAmount = x.RuFilm,
+                ForreignAmount = x.Count - x.RuFilm
             }).AsEnumerable();
             ViewData["TableName"] = "Статистика по жанрам";
             ViewData["Headers"] = new string[] { "Жанр", "Кол-во фильмов снятых в России", "Кол-во фильмов снятых в др. странах" };
---
+++ CInema/Models/Query/StatOnGenre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema.Models.Query
+{
+    public class StatOnGenre
+    {
+        public string Genre { get; set; }
+        public int RusAmount { get; set; }    
+        public int ForreignAmount { get; set; }
+    }
+}
